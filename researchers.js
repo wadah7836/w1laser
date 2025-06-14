38c383c1bdbd8ff30527dea6f782ddf6e0af9e46
wadah7836 <nooraliraqw@gmail.com>
2025-06-14 20:27:02 +0300

    Add files via upload

---
+++ researchers.js
@@ -0,0 +1,246 @@
+const researchers = {
+  "شفيق شاكر شفيق": {
+    name: "أ.د. شفيق شاكر شفيق ",
+    photo: "images/shafik.jpg",
+    bio: "حصل الأستاذ الدكتور شفيق على بكالوريوس علوم الفيزياء من جامعة بغداد بالعراق عام 1998. كما حصل على درجتي الماجستير والدكتوراه في الفيزياء النووية من جامعة بغداد عامي 2001 و 2006 على التوالي. تشمل خبرة شفيق في البحث والتطوير أكثر من 18 عامًا في المجال الأكاديمي والصناعة. أشرف على أكثر من 60 رسالة ماجستير. ودكتوراه. الطلاب في مختلف المجالات",
+    googleScholar: "research.html", // ✅ يفتح الصفحة التي أنشأناها
+
+    achievements: ["عدد البحوث المنشورة", "عدد براءات الاختراع"],
+    education: "دكتوراه في علوم الأحياء الدقيقة",
+    titles: "أستاذ دكتور",
+    positions: "مهارة جيدة في لغة البرمجة ماتلاب والفورتران",
+    awards: "جائزة التميز الأكاديمي 2023",
+
+    educationDetails: [
+      {
+        degree: "دكتوراه",
+        university: "جامعة بغداد",
+        college: "العلوم",
+        country: "العراق",
+        date: "2006-11-15"
+      }
+    ],
+    educationDetails1: [
+      {
+        degree: "كلا",
+                
+        date: "كلا",
+        other: "كلا",
+      }
+    ],
+    educationDetails2: [
+      {
+        name: "شيماء مهدي قدوري",
+                
+        date: "2010",
+        other: "كثافات المستوي الجزئية للنيترون التي تحدث تفاعالت نووية شبه متوازنة",
+      }
+    ],
+
+    educationDetails3: [
+      {
+        "name": "رئيس هيئة البحث العلمي",
+        "date": "2025",
+        "other": ""
+      },
+      {
+        "name": "رئيس جامعة",
+        "date": "2021-08-02",
+        "other": ""
+      },
+      {
+        "name": "مدير تسجيل كلية العلوم - جامعة بغداد",
+        "date": "2007-12-07",
+        "other": "2010-10-01"
+      },
+      {
+        "name": "معاون العميد لشؤون طلبة الدراسات الأولية والعليا / كلية العلوم / جامعة بغداد",
+        "date": "2010-01-10",
+        "other": "2014-08-20"
+      },
+      {
+        "name": "المساعد العلمي لرئيس جامعة الكرخ",
+        "date": "2014-08-20",
+        "other": "2017-07-20"
+      }
+    ],
+    educationDetails4: [
+      {
+        name: "المدالية البرونزية من نادي العلوم الكويتي",
+        other: "مع زميلي ا.د. مهدي هادي وطالب الماجستير في حينه نصير احمد حصلنا الجائزة البرونزية للمبتكرين عن تصميم منظومة انذار مبكر للتلوثات الأشعاعية",        
+        date: "2013-11-18",
+        
+      }
+    ],
+
+    titlesDetails: [
+      {
+        title: "أستاذ",
+        date: "2014-11-27"
+      }
+    ],
+    awards: [
+      {
+        title: "ففف",
+        date: "2014-11-27"
+      }
+    ],
+    personalInfo: {
+      nameAr: "شفيق شاكر شفيق",
+      nameEn: "Prof. Dr. Shafik S. Shafik",
+      birthdate: "1976-09-28",
+      title: "أستاذ",
+      degree: "دكتوراه",
+      major: "علوم الفيزياء",
+      specialization: "الفيزياء النووية",
+      email: "dr.shafik@alayen.edu.iq"
+    },
+    metrics: {
+      gs: {
+        publications: 96,
+        citations: 313,
+        hindex: 8
+      },
+      scopus: {
+        publications: 46,
+        citations: 253,
+        hindex: 8
+      }
+    }
+  },
+
+  "رائد كامل ناجي": {
+    name: "أ.د. رائد كامل ناجي ",
+    photo: "images/raid.jpg",
+    bio: "حصل الأستاذ الدكتور شفيق على بكالوريوس علوم الفيزياء من جامعة بغداد بالعراق عام 1998. كما حصل على درجتي الماجستير والدكتوراه في الفيزياء النووية من جامعة بغداد عامي 2001 و 2006 على التوالي. تشمل خبرة شفيق في البحث والتطوير أكثر من 18 عامًا في المجال الأكاديمي والصناعة. أشرف على أكثر من 60 رسالة ماجستير. ودكتوراه. الطلاب في مختلف المجالات",
+    googleScholar: "research.html", // ✅ يفتح الصفحة التي أنشأناها
+
+    achievements: ["نشر 50 بحثًا", "براءة اختراع"],
+    education: "دكتوراه في علوم الأحياء الدقيقة",
+    titles: "أستاذ دكتور",
+    positions: "مهارة جيدة في لغة البرمجة ماتلاب والفورتران",
+    awards: "جائزة التميز الأكاديمي 2023",
+
+    educationDetails: [
+      {
+        degree: "دكتوراه",
+        university: "جامعة بغداد",
+        college: "العلوم",
+        country: "العراق",
+        date: "2006-11-15"
+      }
+    ],
+    educationDetails1: [
+      {
+        degree: "كلا",
+                
+        date: "كلا",
+        other: "كلا",
+      }
+    ],
+    educationDetails2: [
+      {
+        name: "شيماء مهدي قدوري",
+                
+        date: "2010",
+        other: "كثافات المستوي الجزئية للنيترون التي تحدث تفاعالت نووية شبه متوازنة",
+      }
+    ],
+
+    educationDetails3: [
+      {
+        "name": "المساعد العلمي لهيئة البحث العلمي",
+        "date": "2025",
+        "other": ""
+      },
+      {
+        "name": "",
+        "date": "",
+        "other": ""
+      },
+      {
+        "name": "",
+        "date": "",
+        "other": ""
+      },
+      {
+        "name": "",
+        "date": "",
+        "other": ""
+      },
+      {
+        "name": "",
+        "date": "",
+        "other": ""
+      }
+    ],
+    educationDetails4: [
+      {
+        name: "",
+        other: "",        
+        date: "",
+        
+      }
+    ],
+
+    titlesDetails: [
+      {
+        title: "أستاذ",
+        date: "2014-11-27"
+      }
+    ],
+    awards: [
+      {
+        title: "ففف",
+        date: "2014-11-27"
+      }
+    ],
+    personalInfo: {
+      nameAr: "رائد كامل ناجي",
+      nameEn: "Prof. Dr. Shafik S. Shafik",
+      birthdate: "1976-09-28",
+      title: "أستاذ",
+      degree: "دكتوراه",
+      major: "علوم الفيزياء",
+      specialization: "الفيزياء النووية",
+      email: "dr.shafik@alayen.edu.iq"
+    },
+    metrics: {
+      gs: {
+        publications: 96,
+        citations: 313,
+        hindex: 8
+      },
+      scopus: {
+        publications: 46,
+        citations: 253,
+        hindex: 8
+      }
+    }
+  },
+
+ 
+};
+
+function normalizeName(name) {
+  return name.trim().replace(/\s+/g, ""); // إزالة الفراغات الزائدة لتطبيع الاسم
+}
+
+function searchResearcher() {
+  const inputName = document.getElementById("searchInput").value;
+  const normalizedInput = normalizeName(inputName);
+
+  // ابحث باستخدام التطبيع
+  const matchedKey = Object.keys(researchers).find(key => normalizeName(key) === normalizedInput);
+
+  if (matchedKey) {
+    localStorage.setItem("selectedResearcher", matchedKey);
+    window.location.href = "profile.html";
+  } else {
+    document.getElementById("googleScholar").addEventListener("click", function () {
+      localStorage.setItem("selectedResearcher", researcherKey); // حفظ اسم الباحث
+    });
+    
+
+  }
+}
